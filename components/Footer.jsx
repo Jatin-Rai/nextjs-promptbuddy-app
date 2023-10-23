6c7bf768887a5e2c4db7c58590500cfb6ac1c89b
Jatin-Rai <jatinrai258@gmail.com>
2023-10-23 10:59:39 +0530

    adjusted footer section

--- components/Footer.jsx
+++ components/Footer.jsx
@@ -4,8 +4,9 @@ const Footer = () => {
     const currentYear = new Date().getFullYear();
     return (
         <footer>
-            <p class="mt-8 text-base leading-6 text-center text-gray-400">
+            <p class="my-8 text-base leading-6 text-center text-gray-400">
                 &copy; {currentYear} PromptBuddy. All rights reserved.
+            <span className="absolute bottom-0 right-20 text-xs orange_gradient my-8"> Creator - Jatin Rai</span>
             </p>
         </footer>
     )
